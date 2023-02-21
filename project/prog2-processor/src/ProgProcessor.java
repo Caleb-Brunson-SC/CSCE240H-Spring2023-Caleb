369cfee0b16fe274e356b23a1bb6b7c4b4a3cfcb
SouthernLegion <bones.legion@gmail.com>
2023-02-20 21:25:03 -0500

    test

--- project/prog2-processor/src/ProgProcessor.java
+++ project/prog2-processor/src/ProgProcessor.java
@@ -1,3 +1,10 @@
+/**
+ * ProgProcessor class which takes user input through CLI and provides text response about disease.
+ * @author Caleb Brunson
+ */
+
 public class ProgProcessor {
-    
+    public static void main(String[] args) {
+        System.out.println("Welcome to the ProgProcessor!");
+    }
 }
