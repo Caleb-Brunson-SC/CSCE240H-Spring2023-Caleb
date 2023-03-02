e71cab0439fc7f951ab0ba5263eeb09116295aff
Caleb-Brunson-SC <caleb.brunson.professional@gmail.com>
2023-03-02 00:36:25 -0500

    Updated files

---
+++ project/prog3-ui/src/Driver.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+/**
+ * Driver for the Prog3UI.java class.
+ * @author Caleb Brunson.
+ */
+
+public class Driver {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Welcome to the Prog-3UI!");
+        System.out.println("Enter 'hiv' to learn about the disease:");
+
+        String disease = keyboard.nextLine();
+        if (disease.equalsIgnoreCase("hiv")) {
+            String regexFile = "project\\prog3-ui\\src\\regex-hiv.txt";
+            Prog3UI progCLI = new Prog3UI();
+
+            boolean quit = false;
+            while(!quit) {
+                System.out.println("What would you like to know about HIV?");
+                String userInput = keyboard.nextLine();
+                System.out.println("---------------------------------");
+                progCLI.findRegexMatch(regexFile, userInput);
+                System.out.println("---------------------------------");
+                System.out.println("Enter 'q' to quit or 'c' to continue:");
+                String userChoice = keyboard.nextLine();
+                if (userChoice.equalsIgnoreCase("q") || userChoice.equalsIgnoreCase("quit")) {
+                    quit = true;
+                }
+            }
+        } else {
+            System.out.println("Sorry, invalid input");
+        }
+    }
+}
--- project/prog3-ui/src/Prog3UI.java
+++ project/prog3-ui/src/Prog3UI.java
@@ -4,10 +4,70 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Prog3UI Class
- * @author Caleb Brunson
+ * Prog3UI Class.
+ * @author Caleb Brunson.
  */
 
 public class Prog3UI {
-    
+
+    public static void findRegexMatch(String fileName, String userInput) {
+        try {
+            Scanner sc = new Scanner(new File(fileName));
+            boolean matchFound = false;
+            while (sc.hasNextLine()) {
+                // get the first word from each line
+                String regex = sc.next();
+                // check if there is a pattern match
+                Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+                Matcher matcher = pattern.matcher(userInput);
+                matchFound = matcher.find();
+                if (matchFound) {
+                    String contentFile = sc.next();
+                    printFile(contentFile);
+                }
+                // Check if a next line exists
+                if (sc.hasNextLine()) {
+                    // progress to next line
+                    sc.nextLine(); 
+                }
+            }
+            if (!matchFound) {
+                System.out.println("Sorry, I do not understand that.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean checkFile(String fileName) {
+        File newFile = new File(fileName); 
+
+        if (newFile.length() == 0) { 
+            System.out.println("Sorry! " + fileName + " file is empty.");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static void printFile(String fileName) {
+        try {
+            File myFile = new File(fileName);
+            Scanner myReader = new Scanner(myFile);
+
+            if (!checkFile(fileName)) {
+                myReader.close();
+                return;
+            }
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
 }
