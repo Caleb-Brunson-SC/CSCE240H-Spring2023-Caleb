167a59b4cc86c9f2c42cc3c827ed65cb800d6b6f
Caleb-Brunson-SC <caleb.brunson.professional@gmail.com>
2023-03-22 23:43:38 -0400

    Made ProgIntent a singleton

--- project/prog4-userintent2querymapper/src/ProgIntent.java
+++ project/prog4-userintent2querymapper/src/ProgIntent.java
@@ -1,8 +1,31 @@
+package src;
+import java.io.File;
+import java.util.HashMap;
 /*
  * ProgIntent.java
  * Written by Caleb Brunson
  */
 
 public class ProgIntent {
-    
+    private HashMap<String, File> regexMap = new HashMap<String, File>();
+    private static ProgIntent progIntent;
+
+    private ProgIntent() {
+        regexMap = getRegexMap();
+    }
+
+    public static ProgIntent getInstance() {
+        if(progIntent == null) {
+            progIntent = new ProgIntent();
+        }
+        return progIntent;
+    } 
+
+    public HashMap<String, File> getRegexMap() {
+        return regexMap;
+    }
+
+    public void matchUtterance(String u) {
+        // 
+    }
 }
