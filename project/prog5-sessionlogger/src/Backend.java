94d90847a31e4d4c472ce3a0932483187430d01f
Caleb-Brunson-SC <caleb.brunson.professional@gmail.com>
2023-04-12 09:05:04 -0400

    Updated files

--- project/prog5-sessionlogger/src/Backend.java
+++ project/prog5-sessionlogger/src/Backend.java
@@ -1,5 +1,120 @@
 package src;
+import java.io.File;
+import java.io.BufferedReader;  
+import java.io.FileReader;  
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Backend {
-    
+    private static final String REGEX_PATH = "project\\prog5-sessionlogger\\src\\regex-hiv.csv";
+    private HashMap<String, String> regexMap = new HashMap<String, String>();
+    private static Backend backend;
+
+    private Backend() {
+        this.regexMap = getDataMap();
+    }
+
+    public static Backend getInstance() {
+        if(backend == null) {
+            backend = new Backend();
+        }
+        return backend;
+    } 
+
+    public HashMap<String, String> getDataMap() {
+        // https://stackoverflow.com/questions/4917326/how-to-iterate-over-the-files-of-a-certain-directory-in-java
+        try {
+            // Initalize hashmap
+            HashMap<String, String> regexMap = new HashMap<String, String>();
+            // Read CSV file and add key, value pairs to hashmap
+            BufferedReader br = new BufferedReader(new FileReader(REGEX_PATH));
+            String line = "";  
+            String splitBy = ",";  
+            while ((line = br.readLine()) != null) {  
+                String[] regexStrings = line.split(splitBy); // use comma as separator  
+                regexMap.put(regexStrings[0], regexStrings[1]);
+            }  
+            br.close(); // close the buffered reader
+            // Return the hashmap
+            return regexMap;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public boolean checkFile(String fileName) {
+        File newFile = new File(fileName); 
+
+        if (newFile.length() == 0) { // this line assumes that a file with no characters is empty
+            System.out.println("Sorry! " + fileName + " file is empty.");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void printFile(String fileName) {
+        try {
+            File myFile = new File(fileName);
+            Scanner myReader = new Scanner(myFile);
+
+            if (!checkFile(fileName)) {
+                myReader.close();
+                return;
+            }
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+    }
+
+    public void matchUtterance(String u) {
+        try {
+            // Split strin u into individual words
+            String[] arrOfU = u.split(" ", -2);
+
+            double lowestPercent = 0.0;
+            String resultFilePath = null;
+
+            for (String i : regexMap.keySet()) {
+                String[] regexWords = i.split("\\s+");
+                int regexWordCount = regexWords.length;
+
+                int matchCount = 0;
+
+                for (String uWord : arrOfU) {
+                    for (String regexWord : regexWords) {
+                        if (uWord.equalsIgnoreCase(regexWord)) {
+                            matchCount++;
+                        }
+                    }
+                }
+
+                double matchPercent = matchCount / ((float)regexWordCount);
+                
+                if (matchPercent > lowestPercent) {
+                    lowestPercent = matchPercent;
+                    resultFilePath = regexMap.get(i);
+                }
+            }
+
+            if (lowestPercent > 0.7) {
+                System.out.println("Content from: " + resultFilePath + "; Percent Confidence: " + lowestPercent);
+                printFile(resultFilePath);
+            } else {
+                System.out.println("Sorry, I do not understand that. Please re-phrase your question:");
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
--- project/prog5-sessionlogger/src/Driver.java
+++ project/prog5-sessionlogger/src/Driver.java
@@ -1,5 +1,34 @@
 package src;
+import java.util.Scanner;
 
 public class Driver {
-    
+    public static void main(String[] args) {
+        // Scanner
+        Scanner keyboard = new Scanner(System.in);
+        // Greet user
+        System.out.println("Welcome to the Prog4 Chatbot!");
+        System.out.println("Enter 'hiv' to learn about the disease:");
+        // Get user input for disease type
+        String disease = keyboard.nextLine();
+        if (disease.equalsIgnoreCase("hiv")) {
+            // Loop boolean variable
+            boolean quit = false;
+            // Main loop
+            while (!quit) {
+                System.out.println("What would you like to know about hiv?:");
+                // Instance of ProgIntent class
+                Backend backend = Backend.getInstance();
+                // Get user utterance (u)
+                String u = keyboard.nextLine();
+                // Run query with u
+                backend.matchUtterance(u);
+                // Prompt user to quit
+                System.out.println("Enter 'quit' or 'q' to quit; enter 'c' to continue:");
+                String userChoice = keyboard.nextLine();
+                if (userChoice.equalsIgnoreCase("q") || userChoice.equalsIgnoreCase("quit")) {
+                    quit = true;
+                }
+            }
+        }
+    }
 }
